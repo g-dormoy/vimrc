6e76263d67e1319b5b880f7078c50c7971134b70
Guillaume Dormoy <g.dormoy@pm.me>
2019-08-16 19:33:16 +0200

    add neovim installation to setup script and add information into the readme file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # vimrc
-Vim configuration for faster machine configuration
+
+This repo contains everithing to setup a NeoVim configuration on a brand new ubuntu install
+
+## How to use ?
+
+Download the sources from master
+Extract the files
+Exec the setup file
+
+```bash
+  wget https://github.com/g-dormoy/vimrc/archive/master.tar.gz
+  tar xvzf master.tar.gz
+  ./vimrc-master/setup.sh
+```
+
+Once the setup script done, install the plugins with :PluginInstall
--- setup.sh
+++ setup.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
+
 # neovim configuration file
-FILE=~/.config/nvim/init.vim
+NEOVIM_CNF_FILE=~/.config/nvim/init.vim
 YES=false
 
+# Install Neovim with Python support
+if ! type nvim > /dev/null; then
+  sudo add-apt-repository -y ppa:neovim-ppa/stable
+  sudo apt-get update
+  sudo apt-get -y install neovim python-dev python-pip python3-dev python3-pip
+  sudo pip install --upgrade pip
+  sudo pip3 install --upgrade pip3
+  sudo pip install --user neovim
+  sudo pip3 install --user neovim
+fi
+
+# Install Plug plugin manager
+if [ ! -e "~/.local/share/nvim/site/autoload/plug.vim" ]; then
+  if ! type curl > /dev/null; then
+    sudo apt-get -y install curl
+  fi
+
+   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Setup the configuration
 for arg in "$@"
 do
   if [ "$arg" == "--yes" ] || [ "$arg" == "-y" ]; then
     echo "Replacing the installed configuration"
     YES=true
-    fi
+  fi
   done
 
 
@@ -19,8 +41,9 @@ if [ "$YES" == false ] && [ -f "$FILE" ]; then
     if [ "$resp1" != y ]; then
       echo "Ok, bye!"
       exit
-      fi
-  fi
+    fi
+      
+  rm -rf $NEOVIM_CNF_FILE
+fi
 
-rm -rf $FILE
-ln init.vim $FILE
+cp init.vim $NEOVIM_CNF_FILE
